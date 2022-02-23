3a958c629fbd3663e9180e832e5ece70fd72dce7
= <Jie.Liu4@budweiserapac.com>
2022-02-23 11:21:24 +0800

    a

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen w-screen flex justify-center items-center home ">
     <div class="content-box ">
-      <div class="title text-gray-700 text-3xl font-bold tracking-wide text-center">L - Components</div>
+      <div class="title text-gray-700 text-3xl font-bold tracking-wide text-center">L - Componentss</div>
       <div class="mt-3">
         <div class="px-4 py-1 bg-gray-100  rounded inline-block cursor-pointer  text-blue-500 mr-2 transition hover:shadow" @click="getStarted">Document</div>
         <div class="px-4 py-1 bg-blue-500 rounded inline-block cursor-pointer text-white  transition hover:shadow" @click="getStarted">Get started</div>
